9bf7180236b7a540fa41bdfc2002eb2c8c0ff9a0
DungNv <dungnv66@vingroup.net>
2021-08-30 14:25:07 +0700

    update run test with excel file

--- run_dc_aging.sh
+++ run_dc_aging.sh
@@ -3,4 +3,4 @@ while true; do
   echo "EXECUTE DC-AGING TEST "
   /home/pi/hlc/build/TestSuite/DC_SECC_Charging_Control D
   echo "----END----"
-donew
+done
---
+++ worker.py
@@ -0,0 +1,9 @@
+import os, sys
+import re
+import openpyxl
+
+def main(argv)
+    return 0
+
+if __name__ == "__main__":
+    exit(main(sys.argv))
